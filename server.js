6447670d57f8e291405266618db30202d3afcb2a
Abdalazeezalali <abdalazeezalali387@gmail.com>
2025-11-21 13:29:05 +0300

    first commit

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+//  npm i dotenv to use .env files
+const dotenv = require('dotenv').config();
+const connectDB=require('./config/connectionDB');
+const PORT = process.env.PORT || 3000;
+connectDB();
+// npm i express to create server
+app.use(express.json());
+// npm i cors to connect front-end and back-end
+const cors = require('cors');
+app.use(cors());
+
+app.use("/recipe", require("./routes/recipe"));
+app.use("/user", require("./routes/user"));
+app.use("/public", express.static("public"));
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
